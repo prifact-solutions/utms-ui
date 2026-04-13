fd016553eee03fbfd723ca07e1318b5a67199d75
viswajith-rckr <viswajith@rckr.com>
2026-04-13 18:30:58 +0530

    fix: Add delete option for modules

--- src/app/program-builder/list-modules/list-modules.component.ts
+++ src/app/program-builder/list-modules/list-modules.component.ts
@@ -8,27 +8,46 @@ import { ProgramsService } from 'src/app/programs/services/programs.service';
 @Component({
   selector: 'app-list-modules',
   templateUrl: './list-modules.component.html',
-  styleUrls: ['./list-modules.component.scss']
+  styleUrls: ['./list-modules.component.scss'],
 })
 export class ListModulesComponent extends ComponentBase {
   modules: Module[] = [];
 
-  constructor(private programService: ProgramsService, private route: ActivatedRoute) {
+  constructor(
+    private programService: ProgramsService,
+    private route: ActivatedRoute,
+  ) {
     super();
   }
 
-  public program: Program | null = null;
+  public program!: Program;
 
   ngOnInit() {
-    this.route.params.pipe(
-      switchMap(params => {
-        const programId = params["program_id"];
-        return this.programService.getProgramCatalog(programId);
-      }))
-      .subscribe(program => {
+    this.route.params
+      .pipe(
+        switchMap((params) => {
+          const programId = params['program_id'];
+          return this.programService.getProgramCatalog(programId);
+        }),
+      )
+      .subscribe((program) => {
         this.program = program;
         this.modules = program.modules || [];
       });
   }
 
+  onDelete(moduleId: number): void {
+    const sub = this.programService
+      .deleteModule(this.program.id, moduleId)
+      .pipe(
+        switchMap((_) => {
+          return this.programService.getProgramCatalog(this.program.id);
+        }),
+      )
+      .subscribe((program) => {
+        this.program = program;
+        this.modules = program.modules || [];
+      });
+    this.registerSubscription(sub);
+  }
 }
